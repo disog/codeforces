453295a797191b1258fb7c2fb6fa870065ae4ce1
Diego Sogari <diego.sogari@gmail.com>
2024-08-02 02:31:33 -0300

    [1996] Solve problem F

--- contest/1996/f/main.cc
+++ contest/1996/f/main.cc
@@ -1,4 +1,6 @@
 /**
+ * https://codeforces.com/contest/1996/submission/273966787
+ *
  * (c) 2024 Diego Sogari
  */
 #include <bits/stdc++.h>
@@ -15,6 +17,14 @@ init(__FILE__);
 
 void println(auto &&...args) { ((cout << args << ' '), ...) << endl; }
 
+int binsearch(auto &&f, int s, int e) {
+  while (s < e) {
+    auto m = (s + e + 1) / 2;
+    f(m) ? s = m : e = m - 1;
+  }
+  return e;
+}
+
 template <typename T> struct Num {
   T x;
   Num() { cin >> x; }
@@ -27,23 +37,48 @@ using Int = Num<int>;
 void solve(int t) {
   Int n, k;
   vector<Int> a(n), b(n);
-  multimap<int, int, greater<int>> vals;
+  i64 ans = 0, ck = -k;
+  int amx = 0;
   for (int i = 0; i < n; i++) {
-    vals.emplace(a[i], b[i]);
+    i64 c = (a[i] - 1) / b[i] + 1;
+    ans += c * a[i] - c * (c - 1) * b[i] / 2;
+    amx = max<int>(amx, a[i]);
+    ck += c;
   }
-  vals.emplace(0, 0);
-  i64 ans = 0;
-  for (int i = 0; i < k && vals.size() > 1;) {
-    auto it = vals.begin();
-    auto [val, sub] = *it;
-    auto a = val, b = next(it)->first;
-    i64 c = min(k - i - 1, (a - b) / sub);
-    ans += a * (c + 1) - sub * c * (c + 1) / 2;
-    a -= sub * (c + 1);
-    i += c + 1;
-    vals.erase(it);
-    if (a > 0) {
-      vals.emplace(a, sub);
+  auto f = [&](int m) {
+    i64 count = 0;
+    for (int i = 0; i < n; i++) {
+      auto mn = (a[i] - 1) % b[i] + 1;
+      if (m >= mn) {
+        count += (min<int>(m, a[i]) - mn) / b[i] + 1;
+      }
+    }
+    return count < ck;
+  };
+  auto m = binsearch(f, 1, amx);
+  vector<int> idx(n);
+  iota(idx.begin(), idx.end(), 0);
+  auto cmp = [&](int i, int j) { return b[i] < b[j]; };
+  ranges::sort(idx, cmp);
+  if (m > 0) {
+    for (int i = 0; i < n && ck > 0; i++) {
+      int ai = a[idx[i]], bi = b[idx[i]];
+      auto mn = (ai - 1) % bi + 1;
+      if (m >= mn) {
+        i64 c = min<i64>(ck, (min<int>(m, ai) - mn) / bi + 1);
+        ans -= c * mn + c * (c - 1) * bi / 2;
+        ck -= c;
+      }
+    }
+  }
+  m++;
+  for (int i = 0; i < n && ck > 0; i++) {
+    int ai = a[idx[i]], bi = b[idx[i]];
+    auto mn = (ai - 1) % bi + 1;
+    if (m >= mn && m <= ai && (m - mn) % bi == 0) {
+      i64 c = (m - mn) / bi + 1;
+      ans -= mn + (c - 1) * bi;
+      ck--;
     }
   }
   println(ans);
